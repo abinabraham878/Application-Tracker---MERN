2e6f6595dde959ef6e8ce00c4540de23ce90c023
Abin Abraham <abinabraham878@gmail.com>
2025-03-04 01:34:16 +0100

    added new api for getting single user

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { registerNewUser, userLogin } = require('../controllers/UserController');
+const { registerNewUser, userLogin, getUser } = require('../controllers/UserController');
+const authenticateUser = require('../utils/middlewares/authenticate');
 
 const router = express.Router();
 
 router.post('/register', registerNewUser);
 router.post('/login', userLogin);
+router.get('/getUser/:id', authenticateUser, getUser);
 
 module.exports = router;
